3f12f77c54360b3229eb9de24379db33d2e590b8
William Mittard <billymittard@ymail.com>
2020-04-14 21:14:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  famous_cats << "garfield"
+  (famous_cats << "garfield")
 end
 
 def add_element_to_start_of_array(array, element)
