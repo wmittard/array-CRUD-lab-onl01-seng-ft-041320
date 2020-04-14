27e737585542fe8631d41921f01c62a577789545
William Mittard <billymittard@ymail.com>
2020-04-14 20:58:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,11 +19,11 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  famous_cats.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  famous_cats [0]
 end
 
 def retrieve_first_element_from_array(array)
