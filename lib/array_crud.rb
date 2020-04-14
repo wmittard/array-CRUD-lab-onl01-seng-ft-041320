0fbdf7d15a1e9376a4ba2badf8f66a1de2f7fb25
William Mittard <billymittard@ymail.com>
2020-04-14 21:01:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,9 +27,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  famous_cats
+  famous_cats.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+  famous_cats.last
 end
